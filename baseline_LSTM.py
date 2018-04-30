10eaf53b79d1796c74b04da9591f3a351590f6a4
aclapes <aclapes@gmail.com>
2018-04-30 11:49:32 +0200

    Major changes: LSTM variations can be choosen, along with other options via program arguments

--- baseline_LSTM.py
+++ baseline_LSTM.py
@@ -1,16 +1,17 @@
 import numpy as np
 import os
 import time
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import argparse
 
-from keras.layers import LSTM, Dense, Bidirectional, Dropout, Activation
+from keras.layers import LSTM, Dense, Bidirectional, Activation
 from keras.optimizers import Adam
 from keras import Sequential
 from keras.callbacks import EarlyStopping
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "3"
 
-problems = ['direction', 'sd_su', 'handwave', 'setup', 'f_r_hw_sd_su']
+problems = ['direction','path', 'sd_su', 'handwave', 'setup', 'f_r_hw_sd_su']
 
 def read_dataset_annotation(filepath, mode='row'):
     """
@@ -78,8 +79,12 @@ def pad_temporal(X):
     X_padded_norm = np.empty((len(X), max_len, 500), dtype=X[0].dtype)
     for i,X_i in enumerate(X):
         Xc = X_i - mean_center
-        pad_range = ((max(0, max_len - Xc.shape[0]),0), (0, 0))
-        X_padded_norm[i, :, :] = np.pad(Xc, pad_range, 'constant', constant_values=(0,0))
+        # <--
+        X_padded_norm[i, -Xc.shape[0]:, :] = Xc  # more efficient
+        # ---
+        # pad_range = ((max(0, max_len - Xc.shape[0]),0), (0, 0))
+        # X_padded_norm[i, :, :] = np.pad(Xc, pad_range, 'constant', constant_values=(0,0))
+        # -->
 
     return X_padded_norm
 
@@ -92,22 +97,97 @@ def categorical_to_onehot(y, n_classes, dtype=np.int32):
 
 
 if __name__ == "__main__":
-    annots = read_dataset_annotation('./one_pixel_data_v2.csv', mode='col')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-i',
+        '--input-dir',
+        type=str,
+        dest='input_dir',
+        default='./data/',
+        help=
+        'Dataset directory location (default: %(default)s)')
+
+    parser.add_argument(
+        '-a',
+        '--annotation-filepath',
+        type=str,
+        dest='annotation_filepath',
+        default='./one_pixel_data_v2.csv',
+        help=
+        'Annotation file location (default: %(default)s)')
+
+    parser.add_argument(
+        '-e',
+        '--num_epochs',
+        type=int,
+        dest='num_epochs',
+        default=100,
+        help=
+        'Num epochs (default: %(default)s)')
+
+    parser.add_argument(
+        '-b',
+        '--batch_size',
+        type=int,
+        dest='batch_size',
+        default=32,
+        help=
+        'Batch size (default: %(default)s)')
+
+    parser.add_argument(
+        '-l',
+        '--lstm-variation',
+        type=str,
+        dest='lstm_variation',
+        default='onelayer_lstm',
+        help=
+        'Choose between onelayer_lstm, twolayer_lstm, onelayer_bilstm, twolayer_bilstm (default: %(default))')
+
+    parser.add_argument(
+        '-s',
+        '--hidden-size',
+        type=int,
+        dest='hidden_size',
+        default=256,
+        help=
+        'Hidden size (default: %(default)s)')
+
+    # parser.add_argument(
+    #     '-G',
+    #     '--gpu-memory',
+    #     type=float,
+    #     dest='gpu_memory',
+    #     default=0.24,
+    #     help=
+    #     'GPU memory to reserve (default: %(default)s)')
+
+    parser.add_argument(
+        '-D',
+        '--cuda-devices',
+        type=str,
+        dest='cuda_devices',
+        default="3",
+        help=
+        'GPU devices (default: %(default)s)')
+
+    args = parser.parse_args()
+
+    annots = read_dataset_annotation(args.annotation_filepath, mode='col')
     files = [os.path.join(annot[0].replace("\\",'/'),annot[1])
              for annot in zip(annots['folder'], annots['file_name'])]
 
     train_test_split = annots['train_test_split']
-    earlystopper = EarlyStopping(monitor='val_loss', patience=20, verbose=1)
 
     st_time = time.time()
     try:
-        data = np.load('dataset.npy')
+        data = np.load('/tmp/singlepixel_dataset.npy')
         print 'READING NPY took ', time.time() - st_time, ' secs.'
     except:
-        data = read_dataset('./data/', files)
+        data = read_dataset(args.input_dir, files)
         data = pad_temporal(data)
         print 'READING SEQUENCES (txt) took ', time.time() - st_time, ' secs.'
-        np.save('dataset.npy', data)
+        np.save('/tmp/singlepixel_dataset.npy', data)
 
     for prob in problems:  # iterate over the different problems
         # find valid examples
@@ -124,9 +204,22 @@ if __name__ == "__main__":
 
         # define keras model
         model = Sequential()
-        model.add(Bidirectional(LSTM(256, dropout=0.2, recurrent_dropout=0.2, return_sequences=True),
-                                input_shape=data.shape[1:]))
-        model.add(Bidirectional(LSTM(256, dropout=0.2, recurrent_dropout=0.2)))
+
+        if args.lstm_variation == 'onelayer_lstm':
+            model.add(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2, input_shape=data.shape[1:]))
+        elif args.lstm_variation == 'twolayer_lstm':
+            model.add(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2, return_sequences=True, input_shape=data.shape[1:]))
+            model.add(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2))
+        elif args.lstm_variation == 'onelayer_bilstm':
+            model.add(Bidirectional(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2),
+                                    input_shape=data.shape[1:]))
+        elif args.lstm_variation == 'twolayer_bilstm':
+            model.add(Bidirectional(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2, return_sequences=True),
+                                    input_shape=data.shape[1:]))
+            model.add(Bidirectional(LSTM(args.hidden_size, dropout=0.2, recurrent_dropout=0.2)))
+        else:
+            raise NotImplementedError
+
         model.add(Dense(len(le.classes_)))
         model.add(Activation('softmax'))
         model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001), metrics=['accuracy'])
@@ -135,18 +228,15 @@ if __name__ == "__main__":
         # run train and test
         history = model.fit(data[inds_train],
                             y_train,
-                            validation_data=(data[inds_test], y_test),
-                            batch_size=16,
-                            epochs=100,
+                            # validation_data=(data[inds_test], y_test),
+                            batch_size=args.batch_size,
+                            epochs=args.num_epochs,
                             shuffle=True,
-                            callbacks=[earlystopper],
+                            # callbacks=[EarlyStopping(monitor='val_loss', min_delta=1e-5, patience=20, verbose=1)],
                             verbose=0)
 
         _, te_acc = model.evaluate(data[inds_test], y_test, batch_size=1, verbose=0)
-        print('[Problem] %s : %.4f, %.4f, %.4f' % (prob,
-                                                   history.history['acc'][-1],
-                                                   history.history['val_acc'][-1],
-                                                   te_acc)
+        print('[Problem] %s : %.4f, %.4f' % (prob, history.history['acc'][-1], te_acc)
         )
 
     quit()
